0e4fabfa121cd3423a4bd4c8a1577aeda986c982
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-03-23 01:13:39 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -12,10 +12,20 @@ import Security from "./components/Security";
 import Software from "./components/Software";
 import NavBar from "./components/Nav";
 import ScrollToTop from "./components/ScrollToTop";
+import NotFound from "./components/404";
+
+class EasterEgg extends React.PureComponent {
+  render() {
+    for (let i = 0; i <= 10; i++)
+      console.info("Your awesomeness meter: 0% [" + " █ ".repeat(i) + "   ".repeat(10 - i) + "] 100%")
+    
+    return null;
+  }
+}
 
 function App() {
   let location = useLocation();
-
+  
   return (
     <>
       <ScrollToTop />
@@ -33,9 +43,13 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/security" element={<Security />} />
               <Route path="/software" element={<Software />} />
+
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </CSSTransition>
         </TransitionGroup>
+
+        <EasterEgg />
       </div>
     </>
   );
