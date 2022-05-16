1599ae2541e0c452807e01c51d8102f212f5f226
Zhang Zeyu <zhangzeyu2001@gmail.com>
2022-05-17 03:07:47 +0800

    Fix image URL

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -66,55 +66,62 @@ const Resources = () => {
                             </div>
                         </Col>
                         <Col xs={12} sm={6} md={6} className="justify-content-center align-items-center d-flex flex-column my-5">
-                            <Image src="./img/RVCTF-Talk.jpeg" fluid width="90%" rounded/>
+                            <Image src="/img/RVCTF-Talk.jpeg" fluid width="90%" rounded/>
                         </Col>
                     </Row>
                     <h2 align="center" className='glow'>Notes</h2>
                     <hr/>
-                    <div>
-                        <h3 className="glow">A Levels</h3>
-                        <div className="my-2">
-                            <Tab.Container defaultActiveKey="#link1">
-                            <Row>
-                                <Col sm={6} className="my-2">
-                                    <ListGroup>
-                                        <ListGroup.Item action href="#link1" variant="dark">
-                                            H2 Computing
-                                        </ListGroup.Item>
-                                        <ListGroup.Item action href="#link2" variant="dark">
-                                            H2 Economics
-                                        </ListGroup.Item>
-                                        <ListGroup.Item action href="#link3" variant="dark">
-                                            H1 General Paper
-                                        </ListGroup.Item>
-                                    </ListGroup>
-                                </Col>
-                                <Col sm={6} className="my-2">
-                                <Tab.Content>
-                                    <Tab.Pane eventKey="#link1">
-                                        <p>
-                                            Definitions and qualitative answers.
-                                        </p>
-                                        <a href="/pdf/notes/a-level/9597 Definitions and Qualitative Answers.pdf" target="_blank" rel="noopener noreferrer">Download</a>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="#link2">
-                                        <p>
-                                            Case study question notes.
-                                        </p>
-                                        <a href="/pdf/notes/a-level/Econs CSQ Notes.pdf" target="_blank" rel="noopener noreferrer">Download</a>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="#link3">
-                                        <p>
-                                            Guide to approaching questions and content examples.
-                                        </p>
-                                        <a href="/pdf/notes/a-level/GP Notes.pdf" target="_blank" rel="noopener noreferrer">Download</a>
-                                    </Tab.Pane>
-                                </Tab.Content>
-                                </Col>
-                            </Row>
-                            </Tab.Container>
-                        </div>
-                    </div>
+                    <Row>
+                        <Col className="justify-content-center d-flex flex-column my-5">
+                            <h3 className="glow">A Levels</h3>
+                            <div className="my-2">
+                                <Tab.Container defaultActiveKey="#link1">
+                                <Row>
+                                    <Col sm={6} className="my-2">
+                                        <ListGroup>
+                                            <ListGroup.Item action href="#link1" variant="dark">
+                                                H2 Computing
+                                            </ListGroup.Item>
+                                            <ListGroup.Item action href="#link2" variant="dark">
+                                                H2 Economics
+                                            </ListGroup.Item>
+                                            <ListGroup.Item action href="#link3" variant="dark">
+                                                H1 General Paper
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Col>
+                                    <Col sm={6} className="my-2">
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="#link1">
+                                            <p>
+                                                Definitions and qualitative answers.
+                                            </p>
+                                            <a href="/pdf/notes/a-level/9597 Definitions and Qualitative Answers.pdf" target="_blank" rel="noopener noreferrer">Download</a>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link2">
+                                            <p>
+                                                Case study question notes.
+                                            </p>
+                                            <a href="/pdf/notes/a-level/Econs CSQ Notes.pdf" target="_blank" rel="noopener noreferrer">Download</a>
+                                            <hr/>
+                                            <p>
+                                                Essay content and evaluation points.
+                                            </p>
+                                            <a href="/pdf/notes/a-level/Econs Essay Points and EV.pdf" target="_blank" rel="noopener noreferrer">Download</a>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link3">
+                                            <p>
+                                                Guide to approaching questions and content examples.
+                                            </p>
+                                            <a href="/pdf/notes/a-level/GP Notes.pdf" target="_blank" rel="noopener noreferrer">Download</a>
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                    </Col>
+                                </Row>
+                                </Tab.Container>
+                            </div>
+                        </Col>
+                    </Row>
                 </Container>
             </Container>
         </>
